19258b648d6ad6c98e03e6560e1eb6ee5cc402e1
achyutjagini <achyut.jagini@gmail.com>
2023-05-06 22:09:23 +0530

    a

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: '.env' });
+
+//console.log(process.env.DATABASE)
+/*
+mongoose.connect() is an asynchronous method that returns a promise. When you call
+mongoose.connect() to establish a connection to a MongoDB database, it will 
+return a promise that will resolve when the connection is successfully 
+established or reject if there is an error.*/
+
+// db
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).then(() => console.log('DB Connected'))
+  .catch((error) => console.log("error connecting to DB", error));
+
 
 const cors = require('cors')
 app.use(cors())
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
   res.send("hello world")
-}
-)
+})
 
 app.post("/api/register", (req, res) => {
   console.log(req.body)
@@ -15,7 +34,6 @@ app.post("/api/register", (req, res) => {
 })
 
 const port = 1300
-
 app.listen(port, () => {
   console.log(`server started on ${port}`)
 })
---
+++ server/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true,
+    collection: 'users'
+})
+module.exports = mongoose.model('User', userSchema);
