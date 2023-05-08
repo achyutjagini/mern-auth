7a4d3e3b831d0107b8a2b1e289651309edec418c
achyutjagini <achyut.jagini@gmail.com>
2023-05-08 22:56:19 +0530

    a

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,8 +4,16 @@ const api = axios.create({
     baseURL: 'http://localhost:8000/api',
 })
 
-export const signup = payload => api.post(`/signup`, payload)
-export const signin = payload => api.post(`/signin`, payload)
+export const signup = payload => api.post(`/signup`, payload, {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+export const signin = payload => api.post(`/signin`, JSON.stringify(payload), {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
 
 const apis = {
     signin,
