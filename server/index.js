9c7640974ae8a9b0f22ceff25507c026f65ee7ce
Achyut Jagini <achyut.jagini@gmail.com>
2023-05-11 07:07:49 +0000

    Update 3 files

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config({ path: '.env' });
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: ".env" });
+const helmet = require("helmet");
 
 //http://localhost:8000/api/signup
 //http://localhost:8000/api/signin
 
 //import routes
-const authRoutes = require('./routes/auth');
-const { db } = require('./models/User');
+const authRoutes = require("./routes/auth");
+const { db } = require("./models/User");
 
 //console.log(process.env.DATABASE)
 /*
@@ -19,26 +20,37 @@ return a promise that will resolve when the connection is successfully
 established or reject if there is an error.*/
 
 // db
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-}).then(() => console.log('DB Connected'))
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
   .catch((error) => console.log("error connecting to DB", error));
 
-
-const cors = require('cors')
-app.use(cors())
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'none'"],
+      styleSrc: ["'self'", "https://fonts.googleapis.com"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
+    },
+  })
+);
+const cors = require("cors");
+app.use(cors());
 app.use(bodyParser.json());
 //app.use(express.json())
 
-app.get('/hello', (req, res) => {
-  res.send("hello world")
-})
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
 
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 
-const port = 8000
+const port = 8000;
 app.listen(port, () => {
-  console.log(`server started on ${port}`)
-})
+  console.log(`server started on ${port}`);
+});
